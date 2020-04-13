d2b595912740c61ad22daae1e321c24eae75d68b
Kai Nguyen <kai@zen8labs.com>
2020-04-13 11:15:08 +0700

    Init

---
+++ GreenBuild/GreenBuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GreenBuild
+//
+//  Created by Kai Nguyen on 4/13/20.
+//  Copyright © 2020 Kai Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
